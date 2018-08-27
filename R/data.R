5dd0dbe114a0781fb8583d0f05d0ade0ba393ac2
Philip Waggoner <31326382+pdwaggoner@users.noreply.github.com>
2018-08-27 08:35:08 -0400

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Test Data Frame
+#'
+#' Here is the Roxygen block for documenting our test data.
+#'
+#' @format A data frame with 10 rows and 2 variables:
+#' \describe{
+#'   \item{x}{numeric vector from 1:10}
+#'   \item{carat}{numeric vector from 2:11}
+#' }
+#' @source SSRMC Omnibus Survey
+"df"
